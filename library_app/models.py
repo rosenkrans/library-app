b1dd70ca94001252be1959b004ab3b4e275232c5
Mark Rosenkrans <markrosenkrans@Marks-MacBook-Pro.local>
2019-08-12 14:59:27 -0400

    added models and urls

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=400)
+    isbn = models.CharField(max_length=255)
+    year_published = models.CharField(max_length=4)
+    # artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+
+    def __str__(self):
+        return self.title
+
+class Member(models.Model):
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=14)
+    email = models.CharField(max_length=100)
+    membership_expire_date = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
